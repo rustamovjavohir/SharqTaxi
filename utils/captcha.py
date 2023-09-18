dd9e98148f0a30790e4a77a510cd579747b1113d
rustamovjavohir <rustamovj366@gmail.com>
2023-09-18 16:53:56 +0500

    add captcha

---
+++ utils/captcha.py
@@ -0,0 +1,16 @@
+from io import BytesIO
+
+from PIL import Image
+from captcha.image import ImageCaptcha
+
+
+def generate_captcha(text="Hello captcha", height=220, width=440) -> (Image, BytesIO):
+    """
+    Generate captcha image and returns it with bytes data
+    """
+    captcha: ImageCaptcha = ImageCaptcha(width=width, height=height)
+    data: BytesIO = captcha.generate(text)
+    # captcha.write(text, 'captcha.png')
+    image = Image.open(data)
+    image.show()
+    return image, data
